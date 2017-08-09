336d10c2216a260df8dc64f66d960a7faed45073
cheungth25 <cheungth25@gmail.com>
2017-08-09 17:23:21 -0400

    Done.

--- src/components/PoemWriter.js
+++ src/components/PoemWriter.js
@@ -4,22 +4,49 @@ class PoemWriter extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      textbox: '',
+    };
+  }
+
+  handleTextBox = (event) => {
+    this.setState({
+      textbox: event.target.value
+    })
+  }
+
+  isValidPoem = () => {
+    let lines = this.state.textbox.split(/\n/)
+    debugger
+    if (
+      (lines.length === 3) &&
+      (lines[0].trim().split(/ +/).length === 5) &&
+      (lines[1].trim().split(/ +/).length === 3) &&
+      (lines[2].trim().split(/ +/).length === 5)
+    ){
+      return true
+    }else {
+      return false
+    }
   }
 
   render() {
     return (
       <div>
-        <textarea 
-          rows="3" 
-          cols="60" 
+        <textarea
+          rows="3"
+          cols="60"
+          value={this.state.textbox}
+          onChange={this.handleTextBox}
         />
-        <div 
-          id="poem-validation-error" 
-          style={{color: 'red'}}
-        >
-          This poem is not written in the right structure!
-        </div>
+        {(this.isValidPoem()) ? null :
+          <div
+            id="poem-validation-error"
+            style={{color: 'red'}}
+          >
+            This poem is not written in the right structure!
+          </div>
+         }
       </div>
     );
   }
